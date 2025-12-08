6de3498d6c9d147146769d25de259f32a2569851
Xavier <xavi@arnaus.net>
2025-12-08 13:27:34 +0100

    Try to add the eInk on top

---
+++ kleine/lib/objects/__init__.py
@@ -0,0 +1,8 @@
+from .font_size import FontSize
+from .line import Line
+from .point import Point
+from .rectangle import Rectangle
+from .matrix import Matrix
+from .xproc_action import XprocAction
+from .function_call import FunctionCall, FunctionResponse, FunctionCallPair, FunctionCallHistory
+from .chatbot_response import ChatbotResponse
--- kleine/main.py
+++ kleine/main.py
@@ -1,30 +1,57 @@
 from pyxavi import Config, Dictionary
 from kleine.lib.abstract.pyxavi import PyXavi
 
+from kleine.lib.eink.eink import Eink
+from kleine.lib.eink.macros import Macros
 from kleine.lib.accelerometer.accelerometer import Accelerometer
 from kleine.lib.air_pressure.air_pressure import AirPressure
 from kleine.lib.temperature.temperature import Temperature
 
+import time
+
 class Main(PyXavi):
+
+    eink: Eink = None
+    macros: Macros = None
+    accelerometer: Accelerometer = None
+    air_pressure: AirPressure = None
+    temperature: Temperature = None
 
     def __init__(self, config: Config = None, params: Dictionary = None):
         super(Main, self).init_pyxavi(config=config, params=params)
+
+        # Initialise the eInk display
+        self._xlog.info("Initialising eInk display...")
+        self.eink = Eink(config=self._xconfig, params=self._xparams)
+        self.macros = Macros(config=self._xconfig, params=self._xparams)
+
+        # Initialise the accelerometer
+        self._xlog.info("Initialising accelerometer...")
+        self.accelerometer = Accelerometer(config=self._xconfig, params=self._xparams)
+
+        # Initialise the air pressure sensor
+        self._xlog.info("Initialising air pressure sensor...")
+        self.air_pressure = AirPressure(config=self._xconfig, params=self._xparams)
+
+        # Initialise the temperature sensor
+        self._xlog.info("Initialising temperature sensor...")
+        self.temperature = Temperature(config=self._xconfig, params=self._xparams)
 
     def run(self):
 
         self._xlog.info("🚀 Starting Kleine main run...")
 
+        self.macros.startup_splash(display=self.eink)
+        time.sleep(2)
+
         self._xlog.debug("Test accelerometer...")
-        accelerometer = Accelerometer()
-        accelerometer.test()
+        self.accelerometer.test()
 
         self._xlog.debug("Test air pressure...")
-        air_pressure = AirPressure()
-        air_pressure.test()
+        self.air_pressure.test()
 
         self._xlog.debug("Test temperature...")
-        temperature = Temperature()
-        temperature.test()
+        self.temperature.test()
 
         # However it happened, just close nicely.
         self.close_nicely()
