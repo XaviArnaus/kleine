285dd846cbb227be7f42a2efc57b4103484a88fd
Xavier <xavi@arnaus.net>
2025-12-08 13:31:16 +0100

    Try to add the eInk on top, 3

--- kleine/lib/objects/__init__.py
+++
@@ -1,8 +0,0 @@
-from .font_size import FontSize
-from .line import Line
-from .point import Point
-from .rectangle import Rectangle
-from .matrix import Matrix
-from .xproc_action import XprocAction
-from .function_call import FunctionCall, FunctionResponse, FunctionCallPair, FunctionCallHistory
-from .chatbot_response import ChatbotResponse
