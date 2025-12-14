c014da97d2e22ed8ad6c34259c548baee2e1fa70
Xavier <xavi@arnaus.net>
2025-12-14 08:07:32 +0100

    Ensure screen sizes, 2

--- kleine/lib/lcd/ST7789.py
+++ kleine/lib/lcd/ST7789.py
@@ -4,15 +4,15 @@ from . import lcdconfig
 
 class ST7789(lcdconfig.RaspberryPi):
 
-    width = 320
-    height = 240
+    # width = 320
+    # height = 240
     def command(self, cmd):
         self.digital_write(self.DC_PIN, False)
         self.spi_writebyte([cmd])
     
-    def set_size(self, width, height):
-        self.width = width
-        self.height = height
+    # def set_size(self, width, height):
+    #     self.width = width
+    #     self.height = height
 
     def data(self, val):
         self.digital_write(self.DC_PIN, True)
